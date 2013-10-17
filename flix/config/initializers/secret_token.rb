5f91ff6953a28d56d73f9ee3b725d7d86bb100b0
bullet100 <bullet100@github.com>
2013-10-17 13:45:41 +1100

    Rails first step

---
+++ flix/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flix::Application.config.secret_key_base = '1bb35a9ecf2c19b6f4d842bd21a865d49ee8886a9d14c769c034d4ff740ba3cc6d5b9644cae2aa3adb59f9a4322d9866d0522d8c5511d4a9731098d0eaa38ce3'
---
+++ flix/test/helpers/flix_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class FlixHelperTest < ActionView::TestCase
+end
